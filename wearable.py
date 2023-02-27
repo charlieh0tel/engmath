3c26365876a655c5934e7bcbb751fb50e286adcf
Christopher Hoover <ch@murgatroid.com>
2023-02-27 15:42:33 -0800

    Remove debugging.

--- wearable.py
+++ wearable.py
@@ -9,5 +9,4 @@ TOUCH_CONTINUOUS_TEMP_LIMIT = 43. * pq.C
 def TouchSurfaceFlux(ambient_temp):
     dT = TOUCH_CONTINUOUS_TEMP_LIMIT - ambient_temp
     # Q = h.A.dT => Q/A = h.dT
-    print(f"{heat_transfer.H_PASSIVE}, {dT}, {heat_transfer.H_PASSIVE*dT}")
     return (heat_transfer.H_PASSIVE * dT).rescale(pq.mW / (pq.mm * pq.mm))
