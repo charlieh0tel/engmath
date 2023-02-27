d7348f440031fd83ef3a71a53b1714e4e94ef97c
Christopher Hoover <ch@murgatroid.com>
2023-02-27 14:30:56 -0800

    Initial.

---
+++ battery.py
@@ -0,0 +1,17 @@
+import dataclasses
+import quantities as pq
+
+@dataclasses.dataclass
+class Battery:
+    """Battery properties."""
+    name: str
+    cell_voltage: float
+    specific_energy: float
+    energy_density: float
+
+LithiumNMC = Battery(
+     name="Lithium NMC",
+     cell_voltage=3.6 * pq.V,                # [3.60 - 3.85] V
+     specific_energy=0.600e6 * pq.J / pq.kg, # 0.360–0.954 MJ/kg
+     energy_density=1.70e6 * pq.J / pq.L     # 0.90–2.49 MJ/L
+     )
